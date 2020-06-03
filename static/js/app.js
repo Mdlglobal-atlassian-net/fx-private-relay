6d78f9b609bd9c459ce562ef08b91174ce951d5a
lesley <lnorton@mozilla.com>
2020-06-03 13:01:22 -0500

    Fix header after scrolling

--- static/js/app.js
+++ static/js/app.js
@@ -339,7 +339,18 @@ function watchForInstalledAddon() {
 document.addEventListener("DOMContentLoaded", () => {
 	watchForInstalledAddon();
 	addEventListeners();
-	toggleVisibilityOfElementsIfAddonIsInstalled();
+  toggleVisibilityOfElementsIfAddonIsInstalled();
+
+  const win = window;
+  const header = document.querySelector("header");
+
+  win.onscroll = () => {
+    if (win.pageYOffset > 300) {
+      header.classList.add("fix-header");
+    } else {
+      header.classList.remove("fix-header");
+    }
+  };
 });
 
 class GlocalMenu extends HTMLElement {
